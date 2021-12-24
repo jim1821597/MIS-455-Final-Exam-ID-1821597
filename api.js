51576f6e6d4706c9cd6fa48ecd45f196b5e08116
jim1821597 <1821597@iub.edu.bd>
2021-12-24 11:49:16 +0600

    save

--- api.js
+++ api.js
@@ -5,7 +5,7 @@ var desc = doucument.querySelector('.desc');
 var temp = doucument.querySelector('.temp');
 
 button.addEventListener ('click',function(){
-    fetch('')
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='inputValue.value+'&appid=dfa2010462c6ce0a108a550f199f9756')
 .then(response => response.json())
 .then(data => console.log(data))
 
