b621d3b756cd51724bcba761bea0cfb2c30dea8e
jim1821597 <1821597@iub.edu.bd>
2021-12-24 12:02:13 +0600

    save

--- api.js
+++ api.js
@@ -7,7 +7,16 @@ var temp = doucument.querySelector('.temp');
 button.addEventListener ('click',function(){
     fetch('https://api.openweathermap.org/data/2.5/weather?q='inputValue.value+'&appid=dfa2010462c6ce0a108a550f199f9756')
 .then(response => response.json())
-.then(data => console.log(data))
+.then(data => {
+var nameValue = data['name'];
+var temp = data['main']['temp'];
+var desValue = data['weather'][0]['description']
+
+name.innerHTML =nameValue;
+temp.innerHTML = tempValue;
+desc.innerHTML = descValuconsole;
+
+})
 
 .catch (err => alert("Wrong city name!"))
 })
